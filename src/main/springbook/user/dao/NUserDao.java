073045a416f9820e75c0c4c41f9359b9d14bdbe6
plll0123 <jsh951227@gmail.com>
2022-05-05 19:22:38 +0900

    refactor : 팩토리 메서드 패턴을 적용한 Connection 타입 오브젝트 생성 방법을 확장

---
+++ src/main/springbook/user/dao/NUserDao.java
@@ -0,0 +1,19 @@
+package main.springbook.user.dao;
+
+import main.springbook.ConnectionConst;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static main.springbook.ConnectionConst.*;
+
+public class NUserDao extends UserDao {
+
+    // 커넥션을 가져오는 코드를 상속을 통해 확장
+    @Override
+    public Connection getConnection() throws ClassNotFoundException, SQLException{
+        Connection c = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        return c;
+    }
+}
--- src/main/springbook/user/dao/UserDao.java
+++ src/main/springbook/user/dao/UserDao.java
@@ -5,7 +5,7 @@ import main.springbook.user.domain.User;
 
 import static main.springbook.ConnectionConst.*;
 
-public class UserDao {
+public abstract class UserDao {
 
     /**
      * 커넥션 접근 코드를 메서드로 추출
@@ -56,32 +56,29 @@ public class UserDao {
      * 간단하지만 위험한 테스트 코드 ( DB의 데이터를 직접 삭제하지 않으면 두 번째 부터는 예외가 발생 )
      * id가 하드코딩 되어있다.
      */
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        UserDao dao = new UserDao();
-
-        User user = new User();
-        user.setId("whiteship");
-        user.setName("백기선");
-        user.setPassword("married");
-
-        dao.add(user);
-
-        System.out.println(user.getId() + " 등록 성공");
-
-        User user2 = dao.get(user.getId());
-        System.out.println(user2.getName());
-        System.out.println(user2.getPassword());
-
-        System.out.println(user2.getId() + " 조회 성공");
-    }
+//    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+//        UserDao dao = new UserDao();
+//
+//        User user = new User();
+//        user.setId("whiteship");
+//        user.setName("백기선");
+//        user.setPassword("married");
+//
+//        dao.add(user);
+//
+//        System.out.println(user.getId() + " 등록 성공");
+//
+//        User user2 = dao.get(user.getId());
+//        System.out.println(user2.getName());
+//        System.out.println(user2.getPassword());
+//
+//        System.out.println(user2.getId() + " 조회 성공");
+//    }
 
     /**
-     * 여러 메서드에 중복되어서 나타나는 특정 관심사항이 담긴 코드를 분리함. (Refactoring : extract method)
+     * 메서드를 통한 분리대신 상속을 이용한 확장을 이용
+     * 상속을 통해 서브클래스로 분리함으로써 각자 다른 방식으로 확장이 가능해진다.
      */
-    private Connection getConnection() throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.jdbc.Driver");
-        Connection c = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-        return c;
-    }
+    public abstract Connection getConnection() throws ClassNotFoundException, SQLException;
 
 }
